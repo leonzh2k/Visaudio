1514a6b9c87885e34cdfaf7d6edb0fc135c642a3
leonzh2k <leonzh2k@gmail.com>
2022-06-20 16:39:36 -0700

    continue testing github action

--- .github/workflows/updateProxyURL.py
+++ .github/workflows/updateProxyURL.py
@@ -1,10 +1,13 @@
+
 with open("./js/index.js", "w+") as file:
     fileContents = file.read()
     modifiedFile = fileContents.replace("https://cors-anywhere.herokuapp.com", "https://mighty-stream-75885.herokuapp.com")
+    file.seek(0)
     file.write(modifiedFile)
 
 
 with open("./js/models/vizCreatorModel.js", "w+") as file:
     fileContents = file.read()
     modifiedFile = fileContents.replace("https://cors-anywhere.herokuapp.com", "https://mighty-stream-75885.herokuapp.com")
+    file.seek(0)
     file.write(modifiedFile)
