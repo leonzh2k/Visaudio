07dd7c4a2833872bd8297ac63835df3d43fa1dbc
leonzh2k <leonzh2k@gmail.com>
2022-11-06 00:04:53 +0000

    switch to prod code

--- js/appConfig.js
+++ js/appConfig.js
@@ -2,7 +2,7 @@ const appConfig = {
     BACK4APP_APP_ID: "01t8qb2FLCXC70NIrlplthJEfFpLVhvx6RCK2S2Z",
     BACK4APP_JS_KEY: "MfK5pEk5haJ95TcyTeIkYQdodIQJ2sk1Pn3jZCXX",
     BACK4APP_SERVER_URL: "https://parseapi.back4app.com/",
-    CORS_PROXY_SERVER_URL: "https://cors-anywhere.herokuapp.com",
+    CORS_PROXY_SERVER_URL: "https://cors-anywhere-rzaw.onrender.com",
     NAPSTER_API_KEY: "ZDIxMDM1NTEtYjk3OS00YTI1LWIyYjItYjBjOWVmMWYyN2I3"
 }
 
