b16612397f06630dcfd5b35f1a0f16123d3d9ad1
leonzh2k <leonzh2k@gmail.com>
2022-11-07 13:23:59 -0800

    attempt to fix CORS errors

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+var corsOptions = {
+    origin: process.env.FRONTEND_ORIGIN,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 parse.initialize(process.env.BACK4APP_APP_ID, process.env.BACK4APP_JS_KEY); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 parse.serverURL = process.env.BACK4APP_SERVER_URL;
 
-app.get("/gallery", async (req, res) => {
+app.get("/gallery", cors(corsOptions), async (req, res) => {
     console.log("fetching visualizations...")
     const vizMetadata = parse.Object.extend('vizMetadata');
     const query = new parse.Query(vizMetadata);
